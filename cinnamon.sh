c04f9b9f5edec1a621d070c76746d7cedd275f90
FlsqSpark <76749515+FlsqSpark@users.noreply.github.com>
2022-01-31 09:35:36 -0500

    Add files via upload

---
+++ cinnamon.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+cd Downloads
+
+git clone https://github.com/vinceliuice/Orchis-theme
+
+cd Orchis-theme
+
+./install -t all
+
+cd
+
+cd Downloads
+
+git clone https://github.com/vinceliuice/Tela-circle-icon-theme
+
+cd Tela-circle-icon-theme
+
+./install.sh -a 
+
+cd
+
+cd Downloads
+
+git clone https://github.com/vinceliuice/Vimix-cursors
+
+cd Vimix-cursors
+
+./install.sh
