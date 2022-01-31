db90abba288a5b507d2de0e83e35e425874a6591
FlsqSpark <76749515+FlsqSpark@users.noreply.github.com>
2022-01-31 10:23:33 -0500

    Update cinnamon.sh

--- cinnamon.sh
+++ cinnamon.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg -i google-chrome-stable_current_amd64.deb 
+
+sudo apt-get install -f
+
+wget https://zoom.us/client/latest/zoom_amd64.deb
+
+sudo dpkg -i zoom_amd64.deb
+
+sudo apt-get install -f
+
 cd Downloads
+
+google-chrome https://discord.com/api/download?platform=linux&format=deb
+
+sudo dkpg -i discord-0.0.16.deb
+
+sudo apt-get install -f
 
 git clone https://github.com/vinceliuice/Orchis-theme
 
